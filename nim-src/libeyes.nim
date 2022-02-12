c24f62409e0815f8493b6da7be31eecca3c68178
Nong Hoang Tu <dmknght@parrotsec.org>
2022-02-13 00:31:39 +0700

    add misisng export py

--- nim-src/libeyes.nim
+++ nim-src/libeyes.nim
@@ -11,7 +11,7 @@ proc eyesParseDb*(): seq[string] {.exportpy.} =
   return getDebChecksums()
 
 
-proc eyesCmpDb*(checksum: string, db: seq[string]): bool =
+proc eyesCmpDb*(checksum: string, db: seq[string]): bool {.exportpy.} =
   if checksum in db:
     return true
   return false
